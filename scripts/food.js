347f128f800ec601a5b34bf693d4c22525b69bed
vladimirvikulin <v.vikulin.im11@kpi.ua>
2022-12-23 12:48:24 +0200

    [add] added method randomSuperCoords()

--- scripts/food.js
+++ scripts/food.js
@@ -27,6 +27,11 @@ export default class Food {
     this.y = Food.getRandomFood(0, this.config.rows - 1) * this.config.blockSize;
   }
 
+  randomSuperCoords() {
+    this.superX = Food.getRandomFood(0, this.config.cols - 1) * this.config.blockSize;
+    this.superY = Food.getRandomFood(0, this.config.rows - 1) * this.config.blockSize;
+  }
+
   static getRandomFood(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
